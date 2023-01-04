17057bf3b0d1c92ae27897d8b3a8802a2f542777
prakashtamang <tamangprakash02@gmail.com>
2023-01-04 08:29:47 +0545

    using the logical negation operator

--- program4.cs
+++ program4.cs
@@ -1,9 +1,5 @@
-
-//Console.WriteLine(1 > 2);
-//Console.WriteLine(1 < 2);
-//Console.WriteLine(1 >= 1);
-//Console.WriteLine(1 <= 1);
-//Console.WriteLine(1 > 7);
 string pangram = "The quick brown fox jumps over the lazy dog.";
-Console.WriteLine(pangram.Contains("fox"));
-Console.WriteLine(pangram.Contains("cow"));
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));
+Console.WriteLine(!pangram.Contains("quick"));
+// this code (!) says the text doesnt contain
