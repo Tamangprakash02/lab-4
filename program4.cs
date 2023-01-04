33334249e456f1a881f54813e149ba6b5f988610
prakashtamang <tamangprakash02@gmail.com>
2023-01-04 08:34:25 +0545

    compacting previous code by eliminating temporary variable discount

--- program4.cs
+++ program4.cs
@@ -1,5 +1,4 @@
 int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 70 : 50;
 
-int discount = saleAmount > 1000 ? 70 : 50;
-
-Console.WriteLine($"Discount: {discount}");
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 70 : 50)}");
